f21270a26964dd7bbcf69dd0f874e8edac4957b6
Austin Johnson <technicallyaustin@gmail.com>
2019-12-01 19:18:24 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,15 +15,13 @@ def assembled_aoh
 #KATE_G = { name:  "Kate Gompert", occupation: "None" }
 #BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-new_arr = [
-  DON_G,
-  JOELLE_VD,
-  PAT_M,
-  KATE_G,
-  BRUCE_G
-  ]
-new_arr.sort
-  
+new_arr = [{:name => "Don Gately", :occupation => "Live-in Staff"},
+{:name => "Joelle van Dyne", :occupation => "Radio Personality"},
+{:name => "Pat Monteseian", :occupation => "Staff"},
+{:name => "Kate Gompert", :occupation => "None"}
+{:name => "Bruce Green", :occupation => "Fan of Mildred"}
+]
+  new_arr.sort 
 end
 
 def literal_aoh
